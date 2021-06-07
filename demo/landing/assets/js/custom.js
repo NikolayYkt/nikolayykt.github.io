6a03560677c861819b716e5385155ffd9af2a048
NikolaywebY <Kolyan_top@mail.ru>
2021-06-07 18:06:58 +0900

    ne znau

---
+++ demo/landing/assets/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+
+
+});
