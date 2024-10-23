ef8fa02b3ce7f625b7fb6b7d174b271a8e3a4363
Nikolayweb <45889678+NikolayYkt@users.noreply.github.com>
2024-10-23 20:39:12 +0900

    asd

--- demo/landing/assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-
-
-});
--- demo/mishki/js/custom.js
+++ demo/mishki/js/custom.js
@@ -1,17 +1,18 @@
-    $('.carousel').carousel({
-        interval: false
-    })
-
-   $(".navbar-nav,.navbar-header,.footer-top").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-     if(id=='#main'){$('body,html').animate({scrollTop: 0}, 800);}else{$('body,html').animate({scrollTop: top}, 800);}
+	$(document).ready(function() {
+	//E-mail Ajax Send
+	$("#form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо! Ваше сообщение успешно отправлено!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
 	});
+});
