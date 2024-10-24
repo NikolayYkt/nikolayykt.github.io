c11fae1166278213c85cabd13c619b64c3507971
Nikolayweb <45889678+NikolayYkt@users.noreply.github.com>
2024-10-24 13:10:40 +0900

    1234

---
+++ demo/mishki/wp-content/themes/allium/js/main.js
@@ -0,0 +1,106 @@
+
+
+
+(function($){
+	wp.customize('ads_code', function (value) {
+		value.bind(function (to) {
+		  $('.phone').text(to);
+		});
+	  });
+
+	  wp.customize('ads_code2', function (value) {
+		value.bind(function (to) {
+		  $('.name-m').text(to);
+		});
+	  });
+	  wp.customize('ads_code3', function (value) {
+		value.bind(function (to) {
+		  $('.name-d').text(to);
+		});
+	  });
+	  wp.customize('ads_code7', function (value) {
+		value.bind(function (to) {
+		  $('.call a').text(to);
+		});
+	  });
+	  wp.customize('ads_code8', function (value) {
+		value.bind(function (to) {
+		  $('.home-h1').text(to);
+		});
+	  });
+	  wp.customize('ads_code9', function (value) {
+		value.bind(function (to) {
+		  $('.title h2').text(to);
+		});
+	  });	
+	  wp.customize('ads_code11', function (value) {
+		value.bind(function (to) {
+		  $('.price-01').text(to);
+		});
+	  });	
+	  wp.customize('ads_code13', function (value) {
+		value.bind(function (to) {
+		  $('.price-02').text(to);
+		});
+	  });		
+	  wp.customize('ads_code8', function (value) {
+		value.bind(function (to) {
+		  $('.home-h1 a').text(to);
+		});
+	  });	    	    
+	  wp.customize('ads_code16', function (value) {
+		value.bind(function (to) {
+		  $('.price-03').text(to);
+		});
+	  });	
+	  wp.customize('ads_code18', function (value) {
+		value.bind(function (to) {
+		  $('.price-04').text(to);
+		});
+	  });	
+	  wp.customize('ads_code20', function (value) {
+		value.bind(function (to) {
+		  $('.price-05').text(to);
+		});
+	  });	
+	  wp.customize('ads_code22', function (value) {
+		value.bind(function (to) {
+		  $('.price-06').text(to);
+		});
+	  });
+	  wp.customize('ads_code24', function (value) {
+		value.bind(function (to) {
+		  $('.price-07').text(to);
+		});
+	  });	
+	  wp.customize('ads_code26', function (value) {
+		value.bind(function (to) {
+		  $('.price-08').text(to);
+		});
+	  });	
+	  wp.customize('ads_code28', function (value) {
+		value.bind(function (to) {
+		  $('.price-09').text(to);
+		});
+	  });
+	  wp.customize('ads_code42', function (value) {
+		value.bind(function (to) {
+		  $('.banner-01').text(to);
+		});
+	  });	 
+	  wp.customize('ads_code45', function (value) {
+		value.bind(function (to) {
+		  $('.banner-02').text(to);
+		});
+	  });   	  		    	  
+	  wp.customize('ads_code48', function (value) {
+		value.bind(function (to) {
+		  $('.banner-03').text(to);
+		});
+	  }); 
+	  wp.customize('ads_code51', function (value) {
+		value.bind(function (to) {
+		  $('.footer-01').text(to);
+		});
+	  }); 	  
+ })(jQuery);
